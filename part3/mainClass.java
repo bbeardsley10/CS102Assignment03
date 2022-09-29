fc61bdba0d1803c567c00d6d8e0f901f37ce92d7
Brenton Beardsley <brenton.beardsley@gmail.com>
2022-09-29 15:08:04 -0500

    Create mainClass.java

---
+++ part3/mainClass.java
@@ -0,0 +1,24 @@
+package part3;
+
+public class mainClass {
+    
+    public static void main(String[] args) {
+        
+        String text = "hello everyone. let's write some useful Java code today. we will begin with strings.";
+        // String upperCase = text.substring(0,1).toUpperCase();
+        // String output = upperCase + text.substring(1);
+        // upperCase = upperCase + (Character.toUpperCase(text.charAt(0)) + text);
+
+
+       
+        for(int i = 0; i< text.length(); i++)
+        {
+            if(text.charAt(i) == 0)
+            {
+                i = 
+            }
+        }
+
+        System.out.println(output);
+    }
+}
